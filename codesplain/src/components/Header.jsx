ffea0eebaa7554561cbbf3ad87a596d3cb7d0803
Vandana Kerketta <67150656+Vandy02@users.noreply.github.com>
2026-04-17 16:59:28 +0530

    Update Header.jsx

--- codesplain/src/components/Header.jsx
+++ codesplain/src/components/Header.jsx
@@ -3,8 +3,8 @@
 const Header = () => {
   return (
     <header className="flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold text-white">Codesplain</h1>
-        <p className="text-lg text-white">Explain code in simple terms</p>
+        <h1 className="text-4xl font-bold text-black">Codesplain</h1>
+        <p className="text-lg text-black">Explain code in simple terms</p>
     </header>
   )
 }
